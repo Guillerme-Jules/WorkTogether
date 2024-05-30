552ab783ed74e9745cd1e841213aa9b7644b0325
Jules <Jules.guillerme49@hotmail.com>
2024-05-30 13:01:17 +0200

    Ajout du suivi Matomo

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,10 +24,6 @@ class SecurityController extends AbstractController
         }
         // last username entered by the user
 
-
-
-
-
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
